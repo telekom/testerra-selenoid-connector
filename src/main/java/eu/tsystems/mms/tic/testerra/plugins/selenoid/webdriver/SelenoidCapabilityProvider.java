0da5999ceee029292cf05a5cfbb39b49fd2cd03b
Martin Großmann <martin.grossmann@t-systems.com>
2022-04-29 14:58:04 +0200

    Fixed the getting of current method name

--- src/main/java/eu/tsystems/mms/tic/testerra/plugins/selenoid/webdriver/SelenoidCapabilityProvider.java
+++ src/main/java/eu/tsystems/mms/tic/testerra/plugins/selenoid/webdriver/SelenoidCapabilityProvider.java
@@ -87,9 +87,7 @@ public class SelenoidCapabilityProvider implements Consumer<WebDriverRequest>, L
         final String runConfigName = contextController.getExecutionContext().getRunConfig().RUNCFG;
 
         // Try to find out the current testmethod to add the name to the Selenoid caps
-        String methodName = "";
-        final Optional<Method> optional = ExecutionContextUtils.getInjectedMethod(contextController.getCurrentTestResult().get());
-        methodName = optional.map(Method::getName).orElseGet(() -> contextController.getCurrentMethodContext().get().getName());
+        String methodName = contextController.getCurrentMethodContext().get().getName();
 
         final DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(SelenoidCapabilities.ENABLE_VNC, VNC_ACTIVE);
